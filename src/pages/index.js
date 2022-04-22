2de17eea36c614df47898fd1ea29a9ba28d44d02
Kenny Koch <kennykoch47@gmail.com>
2022-04-22 14:40:09 -0400

    update projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const IndexPage = () => (
             <a href={`mailto:${config.email}`}>{config.email}</a>
           </div>
           <p className="lead mb-5">
-            I'm a software engineer who likes solving<br/>
+            I'm a software engineer who likes solving<br />
             problems and building useful technology.
           </p>
           <div className="social-icons">
@@ -48,14 +48,36 @@ const IndexPage = () => (
           <h2 className="mb-5">Projects</h2>
           <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
             <div className="resume-content">
-              <h3 className="mb-0">NB Cares</h3>
-              <div className="subheading mb-3">A responsive web app enabling the City of New Britain and third-party
-              organizations to inform residents about available resources</div>
+              <h3 className="mb-0">Bike-Safe-Dashboard</h3>
+              <div className="subheading">Data visualization project examining
+                trends in cyclist and pedestrian safety in NYC</div>
+              <div className='text-primary mb-3'>Demo at: <a
+                href="bike-safe-dashboard.herokuapp.com">bike-safe-dashboard.herokuapp.com
+              </a></div>
               <p>
-                In partnership with Central Connecticut State University and the New Britain Housing Authority, I was 
+                The back-end of this project involved using Python to extract and classify relevant
+                information about accidents involving cyclists and/or pedestrians from an approximately
+                1.9 million row dataset provided by NYC Open Data.
+              </p>
+              <p>I then implemented a custom API to serve this data to a React front-end where interactive
+                charts display trends in cyclist and pedestrian safety over time. This site is currently
+                deployed to Heroku, statistics are updated daily.
+              </p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">Mar 2022 - Apr 2022</span>
+            </div>
+          </div>
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="resume-content">
+              <h3 className="mb-0">N.B. Cares</h3>
+              <div className="subheading mb-3">A responsive web app enabling the City of New Britain and third-party
+                organizations to inform residents about available resources</div>
+              <p>
+                In partnership with Central Connecticut State University and the New Britain Housing Authority, I was
                 part of a team which built the New Britain CARES web app. We built a responsive frontend for residents
-                to view relevant community events and filter the events they see based on category/type of event. We also 
-                implemented an interactive event submission form for community organizations and non-profits to submit 
+                to view relevant community events and filter the events they see based on category/type of event. We also
+                implemented an interactive event submission form for community organizations and non-profits to submit
                 events, as well as admin controls for authorized staff to update/edit the interactive events calendar.
               </p>
             </div>
@@ -69,8 +91,8 @@ const IndexPage = () => (
               <div className="subheading mb-3">A browser-based distributed computing platform</div>
               <p>
                 Our capstone project at Fullstack, FortySix allows users to customize and run genetic algorithms and
-                distribute the computationl load across multiple browser clilents. Anyone can contribute CPU resources 
-                to an ongoing computation by opening a url in their browser. 
+                distribute the computationl load across multiple browser clilents. Anyone can contribute CPU resources
+                to an ongoing computation by opening a url in their browser.
               </p>
             </div>
             <div className="resume-date text-md-right">
@@ -152,11 +174,11 @@ const IndexPage = () => (
               Collaboration
             </li>
             <li>
-              <i className="fa-li fa fa-check"></i> 
+              <i className="fa-li fa fa-check"></i>
               Communication
             </li>
             <li>
-              <i className="fa-li fa fa-check"></i> 
+              <i className="fa-li fa fa-check"></i>
               Figuring Things Out
             </li>
           </ul>
@@ -179,7 +201,7 @@ const IndexPage = () => (
                 as well Redux, Node.js, Express.js, and database tools including PostgreSQL and SQLite.
               </p>
               <p> This was one of the most challenging and exciting programs I've ever been a part of, and I'm forever
-                grateful for the opportunity Fullstack provided me to learn alongside so many smart and motivated people.  
+                grateful for the opportunity Fullstack provided me to learn alongside so many smart and motivated people.
               </p>
             </div>
             <div className="resume-date text-md-right">
@@ -192,15 +214,15 @@ const IndexPage = () => (
               <div className="subheading mb-0">B.S. Computer Science</div>
               <div className="text-primary mb-3">Minor in Economics</div>
               <p>At CCSU I studied computer science and economics. During my time there I
-                had the chance to complete several semester-long software projects in 
+                had the chance to complete several semester-long software projects in
                 collaboration with other students, including a 2D game built using the Godot game engine,
                 a terminal-based implementation of the classic game snake written in C, and a web app for the city of New Britain.
               </p>
-              
+
             </div>
             <div className="resume-date text-md-right">
-              <span className="text-primary">Graduated Dec 2021<br/>
-              GPA 3.73</span>
+              <span className="text-primary">Graduated Dec 2021<br />
+                GPA 3.73</span>
             </div>
           </div>
         </div>
